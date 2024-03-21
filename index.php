e027a070845ccd0b9d1c0161bf3824b1c5882987
cRONOS <softcronos@gmail.com>
2024-03-21 17:52:55 -0300

    index update
    copyright on index

--- index.php
+++ index.php
@@ -3,4 +3,13 @@ $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
 $uri = explode( '/', $uri );
 
+echo "API SOLAR soft-cronos.com"
 ?>
+<div class="container">
+    <div class="copyright">
+    © Copyright <strong><span>SOLAR</span></strong>. All Rights Reserved
+    </div>
+    <div class="credits">
+    Created by <a href="https://softcronos.com.ar/">Soft-cRONOS</a>
+    </div>
+</div>
